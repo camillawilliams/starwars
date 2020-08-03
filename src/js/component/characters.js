a3bd25de53f58534f7416bfa71ac66437c7db62c
camillawilliams <camillawilliamsdu@gmail.com>
2020-08-03 23:18:21 +0000

    just before adding api link

---
+++ src/js/component/characters.js
@@ -0,0 +1,28 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+
+// export function Characters(props) {
+// 	//const { image, title, body, url, buttonText } = props;
+// 	return (
+// 		<div className="card">
+// 			<img src={props.image} className="card-img-top" alt="cannot display" />
+// 			<div className="card-body">
+// 				<h5 className="card-title">{props.title}</h5>
+// 				<p className="card-text">{props.body}</p>
+// 				<a href={props.url} className="btn btn-primary">
+// 					{props.buttonText}
+// 				</a>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// Characters.propTypes = {
+// 	image: PropTypes.string,
+// 	title: PropTypes.string,
+// 	body: PropTypes.string,
+// 	buttonText: PropTypes.string,
+// 	url: PropTypes.string
+// };
+
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,87 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom"
+//import { Characters } from "../component/characters.js";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
+import { Row } from "../component/row";
+import { Navbar } from "../component/navbar.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+    return (
+        <div className=“container mt-5">
+            <Row title=“Planets” data={store.planets} />
+        </div>
+    );
+};
+// export function Home() {
+// 	let characters = [
+// 		{
+// 			image: "https://via.placeholder.com/300",
+// 			title: "My card",
+// 			body: "Lorem ipsum dolor sit amet, consectrtur adipicing elit. Nulla rutrum in metus quis pellentesque.",
+// 			buttonText: "View",
+// 			url: "Find out More!"
+// 		},
+// 		{
+// 			image: "https://via.placeholder.com/300",
+// 			title: "My card",
+// 			body: "Lorem ipsum dolor sit amet, consectrtur adipicing elit. Nulla rutrum in metus quis pellentesque.",
+// 			buttonText: "View",
+// 			url: "Find out More!"
+// 		},
+// 		{
+// 			image: "https://via.placeholder.com/300",
+// 			title: "My card",
+// 			body: "Lorem ipsum dolor sit amet, consectrtur adipicing elit. Nulla rutrum in metus quis pellentesque.",
+// 			buttonText: "View",
+// 			url: "Find out More!"
+// 		},
+// 		{
+// 			image: "https://via.placeholder.com/300",
+// 			title: "My card",
+// 			body: "Lorem ipsum dolor sit amet, consectrtur adipicing elit. Nulla rutrum in metus quis pellentesque.",
+// 			buttonText: "View",
+// 			url: "Find out More!"
+// 		}
+// 	];
+
+	// return (
+	// 	<div className="nav-bar">
+	// 		{/* <Navbar brand={navbar.text} /> */}
+	// 		<div className="container">
+	// 			<div className="card-deck">
+	// 				{characters.map((card, index) => {
+	// 					return (
+	// 						<Characters
+	// 							key={index}
+	// 							image={card.image}
+	// 							title={card.title}
+	// 							body={card.body}
+	// 							buttonText={card.buttonText}
+	// 							url={card.url}
+	// 						/>
+	// 					);
+	// 				})}
+	// 			</div>
+	// 			);
+	// 		</div>
+	// 	</div>
+// 	return (
+// 		<div className="container-fluid">
+// 			<div className="row">
+// 				{row.map((card, index) => (
+// 					<div className="col-3" key={index}>
+// 						<Row
+// 							tittle={card.title}
+// 							body={card.body}
+// 							buttonText={card.buttonText}
+// 							url={card.url}
+// 							image={card.image}
+// 						/>
+// 					</div>
+// 				))}
+// 			</div>
+// 		</div>
+// 	);
+// }
